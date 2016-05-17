74d7421a27ebacf8e38f5f68c38d6c455e1dc93f
sidhant goel <sidhant@hexcod.in>
2016-05-17 20:48:41 +0530

    added restcontroller annotation

--- apiservice/src/main/java/in/hexcod/dayapp/controllers/api/IndexController.java
+++ apiservice/src/main/java/in/hexcod/dayapp/controllers/api/IndexController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.web.ErrorAttributes;
 import org.springframework.boot.autoconfigure.web.ErrorController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
@@ -16,6 +17,7 @@ import java.util.Map;
 /**
  * Created by sidhant on 17-05-2016.
  */
+@RestController
 public class IndexController implements ErrorController {
 
     private static final String PATH = "/error";
